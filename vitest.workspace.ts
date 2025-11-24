7a002541cb452cbd7006a575986f7f476afb2d7b
IAmJonoBo <ljcbotha@gmail.com>
2025-11-25 00:09:06 +0200

    chore: add vitest workspace config and refresh exports after lock updates

---
+++ vitest.workspace.ts
@@ -0,0 +1,6 @@
+import { defineWorkspace } from 'vitest/config'
+
+export default defineWorkspace([
+  'n00-cortex/vitest.config.ts',
+  'stuff/frontier-webapp-template/apps/web/vitest.config.ts',
+])
